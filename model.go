88162c412187ec0e39929105d4d96e4da8239fb0
gari8 <hagari.hayato.infratop@gmail.com>
2021-08-16 21:46:43 +0900

    add: response

---
+++ model.go
@@ -0,0 +1,10 @@
+package sprinter
+
+type Response struct {
+	Code int `json:"code"`
+	ContentType string `json:"content_type,omitempty"`
+	Content []byte `json:"content,omitempty"`
+	Text string `json:"text,omitempty"`
+	Object interface{} `json:"object,omitempty"`
+	Path string `json:"path,omitempty"`
+}
